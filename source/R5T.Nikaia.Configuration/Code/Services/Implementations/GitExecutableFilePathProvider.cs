b716fb3c89753c10192c862346687f5b2b686af1
MinexAutomation <minex.automation@gmail.com>
2019-12-12 22:03:50 -0800

    Implemented the Git executable file path provider.

---
+++ source/R5T.Nikaia.Configuration/Code/Services/Implementations/GitExecutableFilePathProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Microsoft.Extensions.Options;
+
+namespace R5T.Nikaia.Configuration
+{
+    public class GitExecutableFilePathProvider : IGitExecutableFilePathProvider
+    {
+        private IOptions<GitConfiguration> GitConfiguration { get; }
+
+
+        public GitExecutableFilePathProvider(IOptions<GitConfiguration> gitConfiguration)
+        {
+            this.GitConfiguration = gitConfiguration;
+        }
+
+        public string GetGitExecutableFilePath()
+        {
+            var gitConfiguration = this.GitConfiguration.Value;
+
+            var gitExecutableFilePath = gitConfiguration.GitExecutableFilePath;
+            return gitExecutableFilePath;
+        }
+    }
+}
