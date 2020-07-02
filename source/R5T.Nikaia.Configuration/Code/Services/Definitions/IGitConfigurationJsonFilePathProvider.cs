8c822db9b9a05e51dc7ef14e03fba215deeee2e8
MinexAutomation <minex.automation@gmail.com>
2020-07-02 00:21:31 -0700

    Changed name of IGitConfigurationJsonFilePathProvider serivce.

---
+++ source/R5T.Nikaia.Configuration/Code/Services/Definitions/IGitConfigurationJsonFilePathProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.Nikaia.Configuration
+{
+    public interface IGitConfigurationJsonFilePathProvider
+    {
+        Task<string> GetGitConfigurationJsonFilePath();
+    }
+}
