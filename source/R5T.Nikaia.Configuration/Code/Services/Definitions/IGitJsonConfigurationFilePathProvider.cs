c4828c417d47ce4e528e705eb2f5910fcc11b48d
MinexAutomation <minex.automation@gmail.com>
2020-07-02 00:09:00 -0700

    Added IGitJsonConfigurationFilePathProvider service definition interface type. Added IServiceCollection extensions.

---
+++ source/R5T.Nikaia.Configuration/Code/Services/Definitions/IGitJsonConfigurationFilePathProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.Nikaia.Configuration
+{
+    public interface IGitJsonConfigurationFilePathProvider
+    {
+        Task<string> GetGitJsonConfigurationFilePath();
+    }
+}
---
+++ source/R5T.Nikaia.Configuration/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+using R5T.Dacia;
+
+
+namespace R5T.Nikaia.Configuration
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="GitExecutableFilePathProvider"/> implementation of <see cref="IGitExecutableFilePathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddGitExecutableFilePathProvider(this IServiceCollection services,
+            IServiceAction<IOptions<GitConfiguration>> gitConfigurationAction)
+        {
+            services
+                .AddSingleton<IGitExecutableFilePathProvider, GitExecutableFilePathProvider>()
+                .Run(gitConfigurationAction)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="GitExecutableFilePathProvider"/> implementation of <see cref="IGitExecutableFilePathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceAction<IGitExecutableFilePathProvider> AddGitExecutableFilePathProviderAction(this IServiceCollection services,
+            IServiceAction<IOptions<GitConfiguration>> gitConfigurationAction)
+        {
+            var serviceAction = ServiceAction.New<IGitExecutableFilePathProvider>(() => services.AddGitExecutableFilePathProvider(
+                gitConfigurationAction));
+
+            return serviceAction;
+        }
+    }
+}
