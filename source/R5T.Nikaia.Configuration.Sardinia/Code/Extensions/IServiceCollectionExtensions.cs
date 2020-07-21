14b05712b17f22773cc3d0c57769348394976f88
MinexAutomation <minex.automation@gmail.com>
2020-07-21 14:40:25 -0700

    Added R5T.Nikaia.Configuration.Base, R5T.Nikaia.Configuration.Sardinia, and R5T.Nikaia.Configuration.Standard libraries.

---
+++ source/R5T.Nikaia.Configuration.Sardinia/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+using R5T.Dacia;
+using R5T.Sardinia;
+
+
+namespace R5T.Nikaia.Configuration
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddGitConfiguration(this IServiceCollection services)
+        {
+            services
+                .Configure<Configuration.Raw.GitConfiguration>()
+                .ConfigureOptions<GitConfigurationConfigureOptions>()
+                ;
+
+            return services;
+        }
+
+        public static IServiceAction<IOptions<GitConfiguration>> AddGitConfigurationAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction<IOptions<GitConfiguration>>.New(() => services.AddGitConfiguration());
+            return serviceAction;
+        }
+    }
+}
